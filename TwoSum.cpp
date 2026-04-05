55323b609719f8fb95e5e7aa55f1bc916c732eb8
Ivan Ibarra <ivanibarra742@gmail.com>
2026-04-04 18:53:19 -0600

    add: TwoSum Problem leetcode

---
+++ TwoSum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> vector = {2, 7, 11, 15};
+    int num = 22, n1 = 0, n2 = 0;
+    for(int i = 0; i < size(vector) - 1; i++){
+	for(int j = i + 1; j < size(vector); j ++){
+	    n1 = vector[i];
+	    n2 = vector[j];
+	    if ((num - n1 - n2) == 0) {
+		cout<<i<<" "<<j<<""<<"\n";
+	    }
+	}
+    }
+    return 0;
+}
