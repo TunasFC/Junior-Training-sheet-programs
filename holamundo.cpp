c56707ae78a47a8dd1b95dc1837e770acef913d9
Ivan Ibarra <ivanibarra742@gmail.com>
2026-04-27 12:43:02 -0600

    add: Program used to test cpp parser

---
+++ holamundo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout<<"Hola mundo";
+    int a = 1;
+    if (a == 1) {
+	cout<<"Probando parser"<<"\n";
+    }else{
+	cout<<"Otra prueba"<<"\n";
+	if(false){
+	    cout<<"Parser de if anidado"<<"\n";
+	    while (false) {
+		int b = 2;
+	    
+	    }
+	}
+	for(int i = 0; i < 1; ++i){
+	    cout<<"Hola"<<"\n";
+	}
+    }
+
+    return 0;
+}
+
